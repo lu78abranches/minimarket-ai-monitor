dcd55cab35ded5974f96302533d02f212aabab8f
lu78abranches <luisabranches.surf@hotmail.com>
2026-04-29 15:31:02 -0300

    repo: adiciona busca de eventos por personId ordenada por data

--- backend-api/src/main/java/com/minimarket/backend_api/repository/EventoRepository.java
+++ backend-api/src/main/java/com/minimarket/backend_api/repository/EventoRepository.java
@@ -1,9 +1,13 @@
 package com.minimarket.backend_api.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.minimarket.backend_api.model.Evento;
 
 public interface EventoRepository extends JpaRepository<Evento, Long> {
+
+    List<Evento> findByPersonIdOrderByTimestampAsc(String personId);
 
 }
